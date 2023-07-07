a45a7bef96d2f522602fb9660f2578cef0c74dae
Will <39478251+VITALISED@users.noreply.github.com>
2023-07-07 20:21:43 +1000

    Add DownloadType.h

---
+++ nms/Atlas/DownloadType.h
@@ -0,0 +1,10 @@
+#pragma once
+
+enum eDownloadType
+{
+	EDownloadType_Unknown = 0x0,
+	EDownloadType_Discovery = 0x1,
+	EDownloadType_Base = 0x2,
+	EDownloadType_Message = 0x3,
+	EDownloadType_NumTypes = 0x4,
+};
